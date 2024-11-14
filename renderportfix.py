d079e00c8604e2c88ee85593359857543df9af99
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2024-11-14 11:01:43 +0530

    Update renderportfix.py

--- renderportfix.py
+++ renderportfix.py
@@ -1,13 +1,25 @@
 import os
-from flask import Flask
+import time
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class Greeting (Resource):
+class Greeting(Resource):
     def get(self):
         return "Frozen is Up & Running!"
 
+class Ping(Resource):
+    def get(self):
+        start_time = time.time()
+        # You could add any processing logic here if needed
+        end_time = time.time()
+        response_time = end_time - start_time
+        return jsonify(message="pong", response_time=f"{response_time:.6f} seconds")
+
 api.add_resource(Greeting, '/')
-app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
+api.add_resource(Ping, '/ping')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
